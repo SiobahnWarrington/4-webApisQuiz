d63404cf1854f3b120bd4263fd2ea8cb92010778
[200~user.name <siobahnwarrington@hotmail.co.uk>
2024-02-11 12:32:14 +0000

    Updated 4 questions for now

--- jscript.js
+++ jscript.js
@@ -4,21 +4,21 @@ var questions = [
     title: 'What is my favourite animal?',
     choices: ['Jellyfish', 'Llama', 'Squirrel', 'Popcorn'],
     answer: 'Squirrel',
-}
+},
 {
     title: 'How many fingers am I holding up?',
-    choices: [1, 3, 77, 12],
+    choices: [1, 3, 8, 12],
     answer: 1,
-}
+},
 {
-    title: 'What is my favourite animal?',
-    choices: ['Jellyfish', 'Llama', 'Squirrel', 'Popcorn'],
-    answer: 'Squirrel',
-}
+    title: 'My favourite movie?',
+    choices: ['The Sound of Music', 'The never ending and eternal void within my soul', '28 Days Later', 'Mrs Doubtfire'],
+    answer: '28 Days Later',
+},
 {
-    title: 'What is my favourite animal?',
-    choices: ['Jellyfish', 'Llama', 'Squirrel', 'Popcorn'],
-    answer: 'Squirrel',
+    title: 'Who is my favourite Author?',
+    choices: ['Douglas Adams', 'Terry Pratchett', 'You', 'Sargent Terry Jeffords of the 99th Precinct'],
+    answer: 'Douglas Adams',
 }
 
 
