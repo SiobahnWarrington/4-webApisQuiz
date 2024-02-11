66e784b9fd5044e913de026ea27f66e403527e0c
[200~user.name <siobahnwarrington@hotmail.co.uk>
2024-02-11 13:26:15 +0000

    adding buttons to HTML and adding audio links for future addition

---
+++ quiz.js
@@ -0,0 +1,31 @@
+//quiz functions, returns, elements and counter
+var startQuiz = document.getElementById("startQuiz")
+
+var welcome = document.getElementById("welcome")
+var quiz = document.getElementById("quiz")
+var result = document.getElementById("result")
+
+var options = document.getElementById("options")
+var message = document.getElementById("message")
+
+var timer = document.getElementById("timer")
+
+var summary = document.getElementById("summary")
+
+
+
+var secondsLeft = 0;
+var score = 0;
+var currentQuestion = 0;
+var countdownTimer;
+
+function stopGame() {
+
+    clearInterval(countdownTimer);
+
+    timer.textContent = ""
+
+    
+
+
+}
