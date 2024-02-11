da7de9d065b3597b3721c9541e251c2f7ca7ba4f
[200~user.name <siobahnwarrington@hotmail.co.uk>
2024-02-11 14:46:42 +0000

    JS not linking with console log  with questions. JS sheets updated and called in body but not working

--- questions.js
+++ questions.js
@@ -1,33 +1,22 @@
 var questions = [
-    //1st set
 {
     title: 'What is my favourite animal?',
     choices: ['Jellyfish', 'Llama', 'Squirrel', 'Popcorn'],
     answer: 'Squirrel',
-},
-{
+},{
     title: 'How many fingers am I holding up?',
     choices: [1, 3, 8, 12],
     answer: 1,
-},
-{
+},{
     title: 'My favourite movie?',
     choices: ['The Sound of Music', 'The never ending and eternal void within my soul', '28 Days Later', 'Mrs Doubtfire'],
     answer: '28 Days Later',
-},
-{
+},{
     title: 'Who is my favourite Author?',
     choices: ['Douglas Adams', 'Terry Pratchett', 'You', 'Sargent Terry Jeffords of the 99th Precinct'],
     answer: 'Douglas Adams',
 }
-
-
-
-
-
-
-
-]
+];
 
 
 //clear interval to stop the timer
